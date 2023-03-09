2dccc227a41d89f9757ee9eb49e91b147b5d2f02
GeovaniFS <geovaniferrarezi@gmail.com>
2023-03-09 09:24:57 -0300

    First commit

---
+++ src/PrimeiroProgramaJava.java
@@ -0,0 +1,12 @@
+import model.Gato;
+
+public class PrimeiroProgramaJava {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 5;
+        System.out.println("Hello world! " + (a+b));*/
+    }
+}
